40a62e5aa1fa455b70ab8de92dd535a5487ec1dd
Tatyana Tarasova <umnova.panther@yandex.ru>
2024-11-01 14:05:43 +0500

    Initial commit

---
+++ src/main/java/ru/netology/demo/config/JavaConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.demo.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.demo.profile.DevProfile;
+import ru.netology.demo.profile.ProductionProfile;
+import ru.netology.demo.profile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true", matchIfMissing = true)
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
---
+++ src/main/java/ru/netology/demo/profile/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.demo.profile;
+
+public interface SystemProfile {
+    String getProfile();
+}
